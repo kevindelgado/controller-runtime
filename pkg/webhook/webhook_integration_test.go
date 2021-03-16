4d1920502f15baf1d38c961efb307818e03bffac
Kevin Delgado <kevindelgado@google.com>
2021-03-16 00:39:03 +0000

    cleanup

--- pkg/webhook/webhook_integration_test.go
+++ pkg/webhook/webhook_integration_test.go
@@ -18,14 +18,44 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
-var _ = Describe("Test", func() {
+var _ = Describe("Webhook", func() {
+	var c client.Client
+	var obj *appsv1.Deployment
+	BeforeEach(func() {
+		Expect(cfg).NotTo(BeNil())
+		var err error
+		c, err = client.New(cfg, client.Options{})
+		Expect(err).NotTo(HaveOccurred())
 
-	Describe("Webhook", func() {
-		BeforeEach(func() {
-			Expect(cfg).NotTo(BeNil())
-		})
+		obj = &appsv1.Deployment{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "apps/v1",
+				Kind:       "Deployment",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-deployment",
+				Namespace: "default",
+			},
+			Spec: appsv1.DeploymentSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{"foo": "bar"},
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"foo": "bar"}},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:  "nginx",
+								Image: "nginx",
+							},
+						},
+					},
+				},
+			},
+		}
+	})
+	Context("when running a webhook server with a manager", func() {
 		It("should reject create request for webhook that rejects all requests", func(done Done) {
-			fmt.Printf("cfg test= %+v\n", cfg)
 			m, err := manager.New(cfg, manager.Options{
 				Port:    testenv.WebhookInstallOptions.LocalServingPort,
 				Host:    testenv.WebhookInstallOptions.LocalServingHost,
@@ -40,36 +70,6 @@ var _ = Describe("Test", func() {
 				_ = server.Start(ctx)
 			}()
 
-			c, err := client.New(cfg, client.Options{})
-			Expect(err).NotTo(HaveOccurred())
-
-			obj := &appsv1.Deployment{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "apps/v1",
-					Kind:       "Deployment",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-deployment",
-					Namespace: "default",
-				},
-				Spec: appsv1.DeploymentSpec{
-					Selector: &metav1.LabelSelector{
-						MatchLabels: map[string]string{"foo": "bar"},
-					},
-					Template: corev1.PodTemplateSpec{
-						ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"foo": "bar"}},
-						Spec: corev1.PodSpec{
-							Containers: []corev1.Container{
-								{
-									Name:  "nginx",
-									Image: "nginx",
-								},
-							},
-						},
-					},
-				},
-			}
-
 			Eventually(func() bool {
 				err = c.Create(context.TODO(), obj)
 				return errors.ReasonForError(err) == metav1.StatusReason("Always denied")
@@ -77,10 +77,10 @@ var _ = Describe("Test", func() {
 
 			cancel()
 			close(done)
-			fmt.Println("PASSED?")
 		})
-
-		It("should reject create request for unmanaged webhook that rejects all requests", func(done Done) {
+	})
+	Context("when running a webhook server without a manager ", func() {
+		It("should reject create request for webhook that rejects all requests", func(done Done) {
 			cluster, err := cluster.New(cfg, func(clusterOptions *cluster.Options) {})
 			Expect(err).NotTo(HaveOccurred())
 
@@ -98,36 +98,6 @@ var _ = Describe("Test", func() {
 				_ = server.Start(ctx)
 			}()
 
-			c, err := client.New(cfg, client.Options{})
-			Expect(err).NotTo(HaveOccurred())
-
-			obj := &appsv1.Deployment{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "apps/v1",
-					Kind:       "Deployment",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-deployment",
-					Namespace: "default",
-				},
-				Spec: appsv1.DeploymentSpec{
-					Selector: &metav1.LabelSelector{
-						MatchLabels: map[string]string{"foo": "bar"},
-					},
-					Template: corev1.PodTemplateSpec{
-						ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"foo": "bar"}},
-						Spec: corev1.PodSpec{
-							Containers: []corev1.Container{
-								{
-									Name:  "nginx",
-									Image: "nginx",
-								},
-							},
-						},
-					},
-				},
-			}
-
 			Eventually(func() bool {
 				err = c.Create(context.TODO(), obj)
 				return errors.ReasonForError(err) == metav1.StatusReason("Always denied")
@@ -135,6 +105,7 @@ var _ = Describe("Test", func() {
 
 			cancel()
 			close(done)
+			fmt.Println("SUCCESS?")
 		})
 	})
 })
@@ -145,68 +116,3 @@ type rejectingValidator struct {
 func (v *rejectingValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
 	return admission.Denied(fmt.Sprint("Always denied"))
 }
-
-//import (
-//	"context"
-//
-//	. "github.com/onsi/ginkgo"
-//	. "github.com/onsi/gomega"
-//	"sigs.k8s.io/controller-runtime/pkg/manager"
-//	. "sigs.k8s.io/controller-runtime/pkg/webhook"
-//	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
-//)
-//
-//var _ = Describe("webhook", func() {
-//
-//	BeforeEach(func() {
-//		Expect(cfg).NotTo(BeNil())
-//	})
-//
-//	Describe("webhook", func() {
-//		It("should serve webhook requests", func(done Done) {
-//			By("Creating the Manager")
-//			mgr, err := manager.New(cfg, manager.Options{})
-//			Expect(err).NotTo(HaveOccurred())
-//
-//			By("Creating the webhook server")
-//			hookServer := &Server{
-//				Port: 8443,
-//			}
-//
-//			By("Building the webhooks")
-//			mutatingHook := &Admission{
-//				Handler: admission.HandlerFunc(func(ctx context.Context, req AdmissionRequest) AdmissionResponse {
-//					return Patched("some changes",
-//						JSONPatchOp{Operation: "add", Path: "/metadata/annotations/access", Value: "granted"},
-//						JSONPatchOp{Operation: "add", Path: "/metadata/annotations/reason", Value: "not so secret"},
-//					)
-//				}),
-//			}
-//
-//			validatingHook := &Admission{
-//				Handler: admission.HandlerFunc(func(ctx context.Context, req AdmissionRequest) AdmissionResponse {
-//					return Denied("none shall pass!")
-//				}),
-//			}
-//
-//			By("registering the webhooks in the server")
-//			hookServer.Register("/mutating", mutatingHook)
-//			hookServer.Register("/validating", validatingHook)
-//
-//			By("adding it to the manager")
-//			if err := mgr.Add(hookServer); err != nil {
-//				panic(err)
-//			}
-//
-//			By("Starting the Manager")
-//			ctx, cancel := context.WithCancel(context.Background())
-//			defer cancel()
-//			go func() {
-//				defer GinkgoRecover()
-//				Expect(mgr.Start(ctx)).NotTo(HaveOccurred())
-//			}()
-//
-//			close(done)
-//		}, 5)
-//	})
-//})
--- pkg/webhook/webhook_suite_test.go
+++ pkg/webhook/webhook_suite_test.go
@@ -18,15 +18,12 @@ package webhook_test
 
 import (
 	"fmt"
-	"net/http"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	admissionv1 "k8s.io/api/admissionregistration/v1"
-	admissionv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -34,7 +31,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 func TestSource(t *testing.T) {
@@ -45,10 +41,6 @@ func TestSource(t *testing.T) {
 
 var testenv *envtest.Environment
 var cfg *rest.Config
-var clientset *kubernetes.Clientset
-
-// clientTransport is used to force-close keep-alives in tests that check for leaks
-var clientTransport *http.Transport
 
 var _ = BeforeSuite(func(done Done) {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
@@ -58,77 +50,23 @@ var _ = BeforeSuite(func(done Done) {
 	var err error
 	cfg, err = testenv.Start()
 	Expect(err).NotTo(HaveOccurred())
-	fmt.Printf("cfg = %+v\n", cfg)
-
-	clientTransport = &http.Transport{}
-	cfg.Transport = clientTransport
-
-	clientset, err = kubernetes.NewForConfig(cfg)
-	Expect(err).NotTo(HaveOccurred())
-
-	// TODO: check if this is needed
-	// Prevent the metrics listener being created
-	metrics.DefaultBindAddress = "0"
-
 	close(done)
 }, 60)
 
 var _ = AfterSuite(func() {
 	fmt.Println("stopping?")
 	Expect(testenv.Stop()).To(Succeed())
-
-	// Put the DefaultBindAddress back
-	metrics.DefaultBindAddress = ":8080"
 }, 60)
 
 func initializeWebhookInEnvironment() {
-	namespacedScopeV1Beta1 := admissionv1beta1.NamespacedScope
 	namespacedScopeV1 := admissionv1.NamespacedScope
-	failedTypeV1Beta1 := admissionv1beta1.Fail
 	failedTypeV1 := admissionv1.Fail
-	equivalentTypeV1Beta1 := admissionv1beta1.Equivalent
 	equivalentTypeV1 := admissionv1.Equivalent
-	noSideEffectsV1Beta1 := admissionv1beta1.SideEffectClassNone
 	noSideEffectsV1 := admissionv1.SideEffectClassNone
 	webhookPathV1 := "/failing"
 
 	testenv.WebhookInstallOptions = envtest.WebhookInstallOptions{
 		ValidatingWebhooks: []client.Object{
-			&admissionv1beta1.ValidatingWebhookConfiguration{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "deployment-validation-webhook-config",
-				},
-				TypeMeta: metav1.TypeMeta{
-					Kind:       "ValidatingWebhookConfiguration",
-					APIVersion: "admissionregistration.k8s.io/v1beta1",
-				},
-				Webhooks: []admissionv1beta1.ValidatingWebhook{
-					{
-						Name: "deployment-validation.kubebuilder.io",
-						Rules: []admissionv1beta1.RuleWithOperations{
-							{
-								Operations: []admissionv1beta1.OperationType{"CREATE", "UPDATE"},
-								Rule: admissionv1beta1.Rule{
-									APIGroups:   []string{"apps"},
-									APIVersions: []string{"v1"},
-									Resources:   []string{"deployments"},
-									Scope:       &namespacedScopeV1Beta1,
-								},
-							},
-						},
-						FailurePolicy: &failedTypeV1Beta1,
-						MatchPolicy:   &equivalentTypeV1Beta1,
-						SideEffects:   &noSideEffectsV1Beta1,
-						ClientConfig: admissionv1beta1.WebhookClientConfig{
-							Service: &admissionv1beta1.ServiceReference{
-								Name:      "deployment-validation-service",
-								Namespace: "default",
-								Path:      &webhookPathV1,
-							},
-						},
-					},
-				},
-			},
 			&admissionv1.ValidatingWebhookConfiguration{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: "deployment-validation-webhook-config",
@@ -167,54 +105,3 @@ func initializeWebhookInEnvironment() {
 		},
 	}
 }
-
-//var cfg *rest.Config
-//var clientset *kubernetes.Clientset
-//
-//// clientTransport is used to force-close keep-alives in tests that check for leaks
-//var clientTransport *http.Transport
-//
-//var _ = BeforeSuite(func(done Done) {
-//	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
-//
-//	err := (&crscheme.Builder{
-//		GroupVersion: schema.GroupVersion{Group: "chaosapps.metamagical.io", Version: "v1"},
-//	}).
-//		Register(
-//			// TODO: dont use controllertest types
-//			&controllertest.UnconventionalListType{},
-//			&controllertest.UnconventionalListTypeList{},
-//		).AddToScheme(scheme.Scheme)
-//	Expect(err).To(BeNil())
-//
-//	testenv = &envtest.Environment{
-//		CRDDirectoryPaths: []string{"testdata/crds"},
-//	}
-//
-//	cfg, err = testenv.Start()
-//	Expect(err).NotTo(HaveOccurred())
-//
-//	clientTransport = &http.Transport{}
-//	cfg.Transport = clientTransport
-//
-//	clientset, err = kubernetes.NewForConfig(cfg)
-//	Expect(err).NotTo(HaveOccurred())
-//
-//	// Prevent the metrics listener being created
-//	metrics.DefaultBindAddress = "0"
-//
-//	close(done)
-//}, 60)
-//
-//var _ = AfterSuite(func() {
-//	Expect(testenv.Stop()).To(Succeed())
-//
-//	// Put the DefaultBindAddress back
-//	metrics.DefaultBindAddress = ":8080"
-//})
-//
-//var _ = BeforeSuite(func(done Done) {
-//	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
-//
-//	close(done)
-//}, 60)
